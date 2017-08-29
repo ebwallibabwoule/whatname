7cb07d93853267c574aa17fc3b475887f7ddc773
Emmanuel Maltete <emmanuel.maltete@finalist.nl>
2017-08-29 21:28:57 +0200

    fixed connection name + cooommmiiicc sans!

--- src/components/user/user.ts
+++ src/components/user/user.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthProvider } from '../../providers/auth';
+import { DataProvider } from '../../providers/data';
 
 @Component({
   selector: 'user',
@@ -10,9 +11,13 @@ export class UserComponent {
   user;
   userData;
 
-  constructor( private auth: AuthProvider ) {
-    this.user = this.auth.getUser().subscribe(user => {
+  constructor( private auth: AuthProvider, private data: DataProvider ) {
+    let userService = this.auth.getUser().subscribe(user => {
       this.user = user;
+      userService.unsubscribe();
+      data.getUserData().subscribe((userData) => {
+        this.userData = userData;
+      });
     });
   }
 
